94d760191013b9219ca9ec1c62dd2f6ac93f1c0c
Wesley Ozebe <33488578+ozebe@users.noreply.github.com>
2020-02-25 21:50:26 -0300

    Update RH.sql
    add tabela funcionario, dependente e jornada de trabalho

--- RH.sql
+++ RH.sql
@@ -80,3 +80,68 @@ FOREIGN KEY (id_profissao) REFERENCES rh_profissao(id), --dados para analise de 
 FOREIGN KEY (id_rh_sit_escol) REFERENCES rh_situacao_escolar(id),
 FOREIGN KEY (id_rh_escol) REFERENCES rh_escolaridade(id)
 );
+
+CREATE TABLE jornada_trabalho(
+id SERIAL NOT NULL UNIQUE,
+descricao VARCHAR(255) NOT NULL,
+criado TIMESTAMP NOT NULL,
+editado TIMESTAMP,
+);
+
+CREATE TABLE dependente(
+id SERIAL NOT NULL UNIQUE,
+id_funcionario INTEGER NOT NULL,
+nome VARCHAR(255),
+cpf VARCHAR(11) NOT NULL,
+data_nasc DATE NOT NULL,
+grau_parentesco VARCHAR(255),
+criado TIMESTAMP NOT NULL,
+editado TIMESTAMP,
+);
+
+CREATE TABLE funcionario(
+id SERIAL NOT NULL UNIQUE,
+id_rh_pessoa_fis INTEGER NOT NULL UNIQUE, --id da tabela rh_pessoa_fisica
+natur VARCHAR(255) NOT NULL, --naturalidade ex: TOLEDO/PR
+est_civ VARCHAR(30) CHECK (est_civ IN ('SOLTEIRO(A)', 'CASADO(A)', 'DIVORCIADO(A)', 'DIVORCIADO(A)', 'VIÚVO(A)', 'SEPARADO JUDICIALMENTE')) NOT NULL, --estadocivil
+nome_pai VARCHAR(255) NOT NULL, --nome do pai
+nome_mae VARCHAR(255) NOT NULL, --nome da mãe ambos como consta no RG
+prim_emprego BOOLEAN NOT NULL, --primeiro emprego?
+pis_pasep VARCHAR(255) NOT NULL, --pis-pasep da carteira de trabalho
+ctps VARCHAR(255), --carteira de trabalho
+ctps_serie VARCHAR(255), --carteira de trabalho serie
+ctps_emissao DATE, --carteira de trabalho emissao
+rg VARCHAR(9) NOT NULL, --rg
+rg_uf VARCHAR(2),
+rg_emissao DATE,
+tit_eleitor VARCHAR(12), --numero do titulo de eleitor
+tit_eleitor_secao VARCHAR(6), --secao do titulo de eleitor
+tit_eleitor_zona VARCHAR(3), --zona do titulo de eleitor
+tit_eleitor_emissao DATE,
+num_habilit VARCHAR(11), --numero da habilitação, se houver
+cat_habilit VARCHAR(5), --categoria da habilitacao EX: AB
+venc_habilit DATE, --data de vencimento da habilitação
+cert_reservista VARCHAR(10), --numero do certificado de reservista
+s_cert_reservista VARCHAR(1),--serie do certificado de reservista
+banco VARCHAR(255) NOT NULL, --dados bancários, banco, ex: Nubank
+agencia VARCHAR(4) NOT NULL, --dados bancários, agencia, ex: 0001
+conta  VARCHAR(20) NOT NULL, --dados bancárias, conta.
+data_admissao DATE NOT NULL, --data de admissão do funcionario
+salario NUMERIC(10,2) NOT NULL, --salário mensal do funcionario
+adicionais NUMERIC(10,2), --adicionais como adicionais noturnos e etc
+obs_adicionais VARCHAR(255) --observações dos adicionais, ex: adicional noturno
+rh_tp_prof_id INTEGER NOT NULL, --profissão do funcionário, nessa tabela tem o tipo e área da profissão, ex: ANALISTA I, TI
+--verificar se o id passado da rh_pessoa_fisica já tem um id_profissao, caso tenha, não pode ser adicionado dois ao cadastrar o funcionário.
+id_jornada_trabalho INTEGER NOT NULL, --jornada de trabalho, ex: 8h diárias, seg a sex.
+experiencia VARCHAR(255), --experiencia de trabalho e etc
+desconto_vr BOOLEAN,
+desctonto_vt BOOLEAN,
+desconto_p_saude BOOLEAN,
+desconto_p_odonto BOOLEAN, 
+val_tot_desco NUMERIC(10,2),
+obs VARCHAR(255), --observações adicionais do funcionario
+PRIMARY KEY(id),
+FOREIGN KEY (rh_tp_prof_id) REFERENCES rh_tipo_profissao(id),
+FOREIGN KEY (id_rh_pessoa_fis) REFERENCES rh_pessoa_fisica(id),
+FOREIGN KEY (id_jornada_trabalho) REFERENCES jornada_trabalho(id)
+);
