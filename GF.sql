df668ec2d6a63d8d2e83ad9b710156f4fac03014
Wesley Ozebe <33488578+ozebe@users.noreply.github.com>
2020-03-08 22:55:36 -0300

    Gestão de Finanças 1.0.3-alpha.2
    Retirado tabelas desnecessários e verificando a viabilidade de novas mudanças.

--- GF.sql
+++ GF.sql
@@ -10,61 +10,17 @@
 --São tabelas destinadas á gestão de finanças, nela existem cadastros de fornecedor, contas a pagar, 
 --contas a receber, plano de contas e etc.
 --PRE-REQUISITOS: ga_usuarios, rh_pessoa_fisica, rh_pessoa_juridica
-
---cadastro de contas, conta bancaria e registro de fluxo de caixa-----------------------------------------
---categoria da conta
-CREATE TABLE gf_categoria_conta(
-id SERIAL NOT NULL UNIQUE,
-descricao VARCHAR(255) NOT null,
-criado TIMESTAMP NOT NULL,
-editado TIMESTAMP,
-PRIMARY KEY(id)
-);
-
---Conta bancaria ou de outro tipo
-CREATE TABLE gf_conta(
-id SERIAL NOT NULL UNIQUE,
-descricao VARCHAR(255) NOT NULL,
-saldo NUMERIC(10,2) NOT NULL,
-idCategoria INTEGER NOT NULL,
-criado TIMESTAMP NOT NULL,
-editado TIMESTAMP,
-PRIMARY KEY(id),
-FOREIGN KEY(idCategoria) REFERENCES gf_categoria_conta(id)
-);
-
---FluxoCaixa
---se for despesa retira o valor do saldo da conta, tentar trigger ?
-CREATE TABLE gf_fluxo_caixa(
-id SERIAL NOT NULL UNIQUE,
-valor NUMERIC(10,2) NOT NULL,
-tipo VARCHAR(13) CHECK (tipo IN ('Despesa', 'Receita', 'Transferencia')) NOT NULL, 
-idConta INTEGER,
-idContaOrigem INTEGER,
-idContaDestino INTEGER,
-situacao VARCHAR(9) CHECK (situacao IN('Efetivado', 'Pago', 'Recebido', 'Aberto')) NOT NULL,
-dataEfetivacao TIMESTAMP,
-dataLancamento TIMESTAMP NOT NULL,
-idUsuario INTEGER NOT NULL,
-observacoes VARCHAR(255),
-PRIMARY KEY(id),
-FOREIGN KEY(idUsuario) REFERENCES ga_usuarios(id),
-FOREIGN KEY(idConta) REFERENCES gf_conta(id),
-FOREIGN KEY(idContaOrigem) REFERENCES gf_conta(id),
-FOREIGN KEY(idContaDestino) REFERENCES gf_conta(id)
-);
-
 ---------------------------------------NOVOS----------------------------------------------------
 --gestão de vendas não vai ter mais: São tabelas destinadas a gestão de vendas, com cadastro de cartões, tipos de pagamento, efetivação de parcelas
 --fara parte da gestão de finanças
 --adicionar: cadastro de bancos, agências e contas correntes
 --PRE-REQUISITOS: RH (rh_pessoa_fisica, rh_pessoa_juridica)
 
---tabela de cadastro de cliente
+--tabela de cadastro de cliente (antes de inserir verificar se existe a pessoa fisica ou juridica)
 CREATE TABLE gf_cliente(
 id SERIAL NOT NULL UNIQUE,
-id_rh_pessoa_fis INTEGER NOT NULL UNIQUE, --id da tabela rh_pessoa_fisica
-id_rh_pessoa_jur INTEGER  UNIQUE, --id da tabela rh_pessoa_juridica
+id_rh_pessoa_fis INTEGER UNIQUE, --id da tabela rh_pessoa_fisica
+id_rh_pessoa_jur INTEGER UNIQUE, --id da tabela rh_pessoa_juridica
 limite_credito NUMERIC(10,2) NOT NULL, --limite de crédito do cliente
 credito NUMERIC(10,2), --credito do cliente, proveniente de estornos devoluções e etc.
 criado TIMESTAMP NOT NULL, --data de criação
@@ -74,6 +30,7 @@ FOREIGN KEY (id_rh_pessoa_fis) REFERENCES rh_pessoa_fisica(id),
 FOREIGN KEY (id_rh_pessoa_jur) REFERENCES rh_pessoa_juridica(id)
 );
 
+--tabela de cadastro de fornecedor, verificar possibilidade de deixar assim ou alterar para rh_pessoa_juridica
 CREATE TABLE gf_fornecedor(
 id SERIAL NOT NULL UNIQUE,
 razao_social VARCHAR(255) NOT NULL,
@@ -118,6 +75,7 @@ PRIMARY KEY(id_plano)
 );
 
 --tabelas onde estão as contas a receber, geradas automaticamente ou manualmente
+--verificar a possibilidade de substituir o num doc para serial
 CREATE TABLE gf_conta_receber(
 id SERIAL NOT NULL UNIQUE,
 num_doc VARCHAR(30) NOT NULL UNIQUE, --número do documento gerado na conta a receber
@@ -131,6 +89,7 @@ FOREIGN KEY (id_gf_plan_contas) REFERENCES gf_plano_contas(id_plano)--plano de c
 );
 
 --tabelas onde estão as contas a pagar, geradas automaticamente ou manualmente
+--verificar a possibilidade de substituir o num doc para serial
 CREATE TABLE gf_conta_pagar(
 id SERIAL NOT NULL UNIQUE,
 num_doc VARCHAR(30) NOT NULL UNIQUE, --número do documento gerado na conta a pagar
@@ -178,6 +137,8 @@ PRIMARY KEY(id),
 FOREIGN KEY (id_gf_conta_pagar) REFERENCES gf_conta_pagar(id) 
 );
 
+--abaixo são tabelas em teste para as contas onde serão realizadas as movimentações, tanto de caixa
+--como para pagamento de duplicatas e fornecedores.
 CREATE TABLE gf_banco(
 cod_banco VARCHAR(5) NOT NULL UNIQUE, --codigo do banco, ex: 260
 nome_banco VARCHAR(100) NOT NULL, --nome do banco, ex: Nubank
